390cc3cbad67e33c4d9d37df6d6f81a16b5ea229
Eliberto Subias <eliberto.subias.ortega@gmail.com>
2024-09-08 19:13:44 -0700

    Se configura puerto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: false,
+  appBaseUrl: "http://localhost:8001/api",
+  appBaseUrlMedia: "http://localhost:8001/uploads",
+  configFile: "assets/settings/settings.json"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  appBaseUrl: "http://localhost:4000/api",
-  appBaseUrlMedia: "http://localhost:4000/uploads",
+  appBaseUrl: "http://localhost:8001/api",
+  appBaseUrlMedia: "http://localhost:8001/uploads",
   configFile: "assets/settings/settings.json"
 };
 
